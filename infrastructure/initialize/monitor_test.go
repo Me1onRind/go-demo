d5aa8d4d3f660e8373a8654c7bdf2d9817c6562b
lening.zeng <lening.zeng@shopee.com>
2022-02-02 14:06:32 +0800

    unit test

--- infrastructure/initialize/monitor_test.go
+++ infrastructure/initialize/monitor_test.go
@@ -1,8 +1,13 @@
 package initialize
 
 import (
+	"bufio"
+	"bytes"
 	"context"
+	"errors"
 	"fmt"
+	"io"
+	"sync"
 	"time"
 
 	//"net/http"
@@ -19,13 +24,38 @@ import (
 func TestPrometheusServer(t *testing.T) {
 	InitPrometheusServer()
 	time.Sleep(time.Millisecond * 10)
+
+	total := 10
+	totalStr := fmt.Sprintf("request_total{method=\"test/method\"} %d", total)
+
 	f := middleware.GrpcMonitor()
-	f(context.Background(), nil, &grpc.UnaryServerInfo{
-		FullMethod: "test/method",
-	}, func(ctx context.Context, req interface{}) (interface{}, error) { return nil, nil })
+	wg := &sync.WaitGroup{}
+	for i := 0; i < total; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			f(context.Background(), nil, &grpc.UnaryServerInfo{
+				FullMethod: "test/method",
+			}, func(ctx context.Context, req interface{}) (interface{}, error) { return nil, nil })
+		}()
+	}
+	wg.Wait()
 	client := resty.New()
 	resp, err := client.R().Get(fmt.Sprintf("http://%s/metrics", config.RemoteConfig.Prometheus.Addr()))
 	if assert.Empty(t, err) {
-		fmt.Printf("%s\n", resp.Body())
+		buf := bufio.NewReader(bytes.NewBuffer(resp.Body()))
+		for {
+			line, _, err := buf.ReadLine()
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			if assert.Empty(t, err) {
+				if string(line) == totalStr {
+					t.Logf("%s", line)
+					return
+				}
+			}
+		}
+		t.Fatalf("Prometheus Server not response %s", totalStr)
 	}
 }
